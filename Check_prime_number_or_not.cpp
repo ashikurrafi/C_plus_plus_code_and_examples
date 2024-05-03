a11b11b2dd1cd438cfa384d7290c3c99302fab3b
Ashikur Rafi <ashikurrafi@gmail.com>
2024-05-04 02:25:58 +0600

    Add C++ program to check a number is prime or not
    This commit introduces a C++ program named "Prime Number Checker" that determines whether a given integer is a prime number or not. The program utilizes a class called "number" with methods for setting data and checking whether the number is prime. It takes user input, sets the data, and then checks whether the number is prime or not using a simple algorithm. This commit provides a basic implementation of the prime number checking functionality.

---
+++ Check_prime_number_or_not.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+class number
+{
+public:
+    number(void);
+    number(int n);
+    int set_data(int num);
+    void check_prime_or_not();
+
+    ~number();
+
+private:
+    int x;
+};
+
+number::number(void)
+{
+    x = 0;
+}
+
+number::number(int n)
+{
+    x = n;
+}
+
+int number::set_data(int num)
+{
+    return x = num;
+}
+
+void number::check_prime_or_not()
+{
+    int flag = 0;
+    for (int i = 2; i < x / 2; i++)
+    {
+        if (x % 2 == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (x == 0)
+    {
+        cout << "0 is neither prime nor composite ";
+    }
+    else if (x == 1)
+    {
+        cout << "1 is neither prime nor composite ";
+    }
+    else if (flag == 0)
+    {
+        cout << x << " is a prime number";
+    }
+    else
+    {
+        cout << x << " is not a prime number";
+    }
+}
+
+number::~number()
+{
+}
+
+int main()
+{
+    int data;
+    number object;
+    cout << "Enter a number to check prime or not : ";
+    cin >> data;
+    object.set_data(data);
+    object.check_prime_or_not();
+    return 0;
+}
