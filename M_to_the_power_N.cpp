924ffbc46a0473215ff230eb3c0bb68087c2942b
Ashikur Rafi <ashikurrafi@gmail.com>
2024-05-09 23:13:26 +0600

    Add C++ program to calculate power of a number
    This commit adds a C++ program that calculates the power of a number using the pow function from the <cmath> library. The program prompts the user to enter two integers, m and n, representing the base and exponent respectively. It then computes and displays m raised to the power of n. This code can be useful for various mathematical applications where exponentiation is required.

---
+++ M_to_the_power_N.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int m;
+    int n;
+    cout << "Enter value: of m : ";
+    cin >> m;
+    cout << "Enter value: n : ";
+    cin >> n;
+    cout << "M to the power N = " << pow(m, n);
+    return 0;
+}
